3750a70a44ff45b15c0f640f6322a3f25bf09a1d
Paolo <p.fiaschi@hotmail.it>
2020-09-16 11:30:49 +0200

    attributi e []

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -3,22 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <button (click)="toggle()">Toggle</button>
-    <h1 *ngIf="visible">Hello World!!</h1>
-    <li
-    [hidden]="!visible"
-    *ngFor="let user of users">
-      {{user}}
-    </li>
+    <button [disabled]="url" (click)="load()">ADD</button>
+    <button [disabled]="!url" (click)="onLoad()">REMOVE</button>
+    <img *ngIf="url" [src]="url">
+    <div [innerHTML]="url"></div>
   `,
   styles: [`
 
   `]
 })
 export class AppComponent {
-  visible = true;
-  users = ['Mattia', 'Paolo', 'Bene'];
-  toggle() {
-    this.visible = !this.visible;
+  url: string;
+  load(): void{
+    this.url = 'https://angular.io/assets/images/logos/angular/angular.png';
+  }
+  onLoad(): void{
+    this.url = null;
   }
 }
