8f7806a660140e9c0c6e276146a2cc8945603934
Paolo <p.fiaschi@hotmail.it>
2020-09-16 16:13:05 +0200

    custom types

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -1,37 +1,25 @@
 import { Component } from '@angular/core';
+import { Users } from './model/users';
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>I PIPES</h1>
-   <p>{{today | date: 'dd/MM/yyyy'}}</p>
-   <p>{{money | currency: '€'}}</p>
-   <p>lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum
-     {{bitcoins | number: '1.2-4'}}</p>
-   <pre>{{user | json}}</pre>
-
-   <h2>Pipe Truncate</h2>
-   <li *ngFor="let art of article">
-     <h3>{{art.titolo}}</h3>
-     <i>{{art.autore}}</i>
-     <p>{{art.testo | truncate: 100}}</p>
-   </li>
+  <li *ngFor="let user of users">
+    {{user.id}} - {{ user.name}} - {{user.age}}
+  </li>
   `,
   styles: [`
 
   `]
 })
 export class AppComponent {
-  article = [
-    {
-      id: 1,
-      titolo: 'Titolo',
-      autore: 'Paolo',
-      testo: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum, nesciunt consequuntur. Inventore nam amet dignissimos delectus, optio alias velit voluptatem enim sint earum, hic explicabo placeat nisi beatae, ex nulla.'
-    }
+  label:boolean;
+  constructor(){
+    this.label = true;
+  }
+  users: Users[] = [
+    {id: 1, name: 'Paolo', age: 32},
+    {id: 2, name: 'Bene', age: 32},
+    {id: 3, name: 'Luca', age: 32},
   ];
-  today = Date.now();
-  money = 1200;
-  bitcoins = 0.1452587;
-  user = {id: 1, name: 'Paolo'};
 }
