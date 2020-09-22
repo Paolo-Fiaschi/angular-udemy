aa8ab30eae24ed6ee57971e9fba33104f53a2709
Paolo <p.fiaschi@hotmail.it>
2020-09-22 18:38:06 +0200

    activatedRoute e router

--- myApp/src/app/feauters/user-detail/user-detail.component.ts
+++ myApp/src/app/feauters/user-detail/user-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 
@@ -12,27 +12,36 @@ import { HttpClient } from '@angular/common/http';
       </h1>
       <h2>{{user.email}}</h2>
     </div>
+    <button class="btn btn-primary" (click)="next()">Next</button>
   `,
   styles: [
   ]
 })
 export class UserDetailComponent implements OnInit {
   user: any;
+  id: number;
   constructor(
     private activatedRoute: ActivatedRoute,
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router
     ) {
+      console.log('crt');
 
+    }
+    next() {
+      const nextId = this.id + 1;
+      this.router.navigateByUrl(`user/${nextId}`)
     }
 
     ngOnInit(): void {
-    const id = +this.activatedRoute.snapshot.params.id;
-    this.http.get(`https://jsonplaceholder.typicode.com/users/${id}`).
-    subscribe(res => {
-      console.log(res);
-      this.user = res;
-
-    });
+      this.activatedRoute.params.subscribe(params => {
+        this.id = +params.id;
+        this.http.get(`https://jsonplaceholder.typicode.com/users/${this.id}`).
+        subscribe(res => {
+          console.log(res);
+          this.user = res;
+        });
+      })
   }
 
 }
